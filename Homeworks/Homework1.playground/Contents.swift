a1b8c8ec0ec85de6a2cd6eabca20f5d47adc9fb7
lapikag <lapikag@gmail.com>
2024-07-13 16:18:33 +0200

    Initial commit

---
+++ Homeworks/Homework1.playground/Contents.swift
@@ -0,0 +1,51 @@
+//Домашнее задание
+//
+//Задача 1
+//
+//for number in 0...100 {
+//    if number % 2 == 0 {
+//        print(number)
+//    }
+//}
+//
+//Задача 2
+//
+//let x = 7
+//let y = 20
+//
+//if x * y > 10 {
+//    print(x)
+//} else {
+//    print(y)
+//}
+//
+
+//Задача 3
+
+//var k = 9
+//
+//for number in 10...50 {
+//    if number % 2 != 0 {
+//        if (number+k) % 2 == 0 {
+//            k += 2
+//        } else {
+//            print ("Next")
+//        }
+//    }
+//}
+
+//Задача 4
+
+//var k = 9
+//
+//for number in 10...50 {
+//    if number % 2 != 0 {
+//        if (number+k) % 2 == 0 {
+//            k += 1
+//            print(k)
+//        } else {
+//            print ("Next")
+//            break
+//        }
+//    }
+//}
