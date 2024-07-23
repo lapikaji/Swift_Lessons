ef0a922ac7a58cafc7c4736d9882fee011421275
lapikag <lapikag@gmail.com>
2024-07-23 19:29:36 +0200

    updated Homework4

--- Homeworks/Homework4.playground/Contents.swift
+++ Homeworks/Homework4.playground/Contents.swift
@@ -1,36 +1,33 @@
 import Foundation
 
-enum PizzaType: String {
-    case margherita = "Margherita"
-    case pepperoni = "Pepperoni"
-    case marinara = "Marinara"
-    case capricciosa = "Capricciosa"
-    case quattroFormaggi = "Quattro Formaggi"
-}
-
-enum Extras: String {
-    case sausage = "Pepperoni"
-    case tomatoes = "Dried Tomatoes"
-    case cheese = "Mozarella"
-    case mushrooms = "Mushrooms"
-    case ham = "Mortadella"
-}
-
-enum PizzaSize: String {
-    case grande = "Grande"
-    case piccola = "Piccola"
-}
-
 struct Pizza {
+    enum PizzaType: String {
+        case margherita = "Margherita"
+        case pepperoni = "Pepperoni"
+        case marinara = "Marinara"
+        case capricciosa = "Capricciosa"
+        case quattroFormaggi = "Quattro Formaggi"
+    }
+    enum Extras: String {
+        case sausage = "Pepperoni"
+        case tomatoes = "Dried Tomatoes"
+        case cheese = "Mozarella"
+        case mushrooms = "Mushrooms"
+        case ham = "Mortadella"
+    }
+    enum PizzaSize: String {
+        case grande = "Grande"
+        case piccola = "Piccola"
+    }
+    enum CrustType: String {
+        case thick = "Thick"
+        case thin = "Thin"
+    }
     var price: Double
     var type: PizzaType
-    var isThickCrust: Bool
     var toppings: [Extras]
     var size: PizzaSize
-    
-    var crustDescription: String {
-        return isThickCrust ? "thick crust" : "thin crust" // Не очень нравятся "сырые" True/False на выходе, поэтому немного преобразовал их. В качестве альтернативного способа можно было сделать перечисление (сделал так для размера пиццы, строки 19-22)
-    }
+    var crust: CrustType
 }
 
 class Pizzeria {
@@ -45,24 +42,24 @@ class Pizzeria {
     }
     
     func getPizzas() -> [Pizza] {
-        return availablePizzas
+        availablePizzas
     }
 }
 
-let pizza1 = Pizza(price: 4.99, type: .margherita, isThickCrust: false, toppings: [.cheese, .tomatoes], size: .grande)
-let pizza2 = Pizza(price: 6, type: .pepperoni, isThickCrust: true, toppings: [.sausage, .cheese], size: .piccola)
-let pizza3 = Pizza(price: 6.99, type: .marinara, isThickCrust: false, toppings: [.cheese, .tomatoes], size: .grande)
-let pizza4 = Pizza(price: 8, type: .capricciosa, isThickCrust: true, toppings: [.mushrooms, .ham, .cheese], size: .piccola)
-let pizza5 = Pizza(price: 8.99, type: .quattroFormaggi, isThickCrust: false, toppings: [.cheese], size: .grande)
+let pizza1 = Pizza(price: 4.99, type: .margherita, toppings: [.cheese, .tomatoes], size: .grande, crust: .thin)
+let pizza2 = Pizza(price: 6, type: .pepperoni, toppings: [.sausage, .cheese], size: .piccola, crust: .thick)
+let pizza3 = Pizza(price: 6.99, type: .marinara, toppings: [.cheese, .tomatoes], size: .grande, crust: .thin)
+let pizza4 = Pizza(price: 8, type: .capricciosa, toppings: [.mushrooms, .ham, .cheese], size: .piccola, crust: .thick)
+let pizza5 = Pizza(price: 8.99, type: .quattroFormaggi, toppings: [.cheese], size: .grande, crust: .thin)
 
 let pizzeria = Pizzeria(pizzas: [pizza1, pizza2, pizza3, pizza4, pizza5])
 
-    let newPizza = Pizza(price: 7, type: .pepperoni, isThickCrust: false, toppings: [.sausage, .mushrooms, .cheese], size: .grande)
+    let newPizza = Pizza(price: 7, type: .pepperoni, toppings: [.sausage, .mushrooms, .cheese], size: .grande, crust: .thin)
 pizzeria.addPizza(newPizza)
 
 print("Special offer!")
 
 let allPizzas = pizzeria.getPizzas()
 for pizza in allPizzas {
-    print("\(pizza.type.rawValue) \(pizza.size.rawValue) with \(pizza.crustDescription) and lovely toppings (\(pizza.toppings.map { $0.rawValue }.joined(separator: ", "))) for only \(pizza.price) EUR!")
+    print("\(pizza.type.rawValue) \(pizza.size.rawValue) with \(pizza.crust) crust and lovely toppings (\(pizza.toppings.map { $0.rawValue }.joined(separator: ", "))) for only \(pizza.price) EUR!")
 }
